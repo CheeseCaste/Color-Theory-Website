95c5684da924cc843b376305df8fc79075742b55
CheeseCaste <elexiscastaneda@gmail.com>
2024-10-04 22:30:38 +0800

    ya

--- START/start.js
+++ START/start.js
@@ -1,8 +1,27 @@
+const chattext = document.getElementById('chattext');
+const Who = document.getElementById('Who');
+const Start = document.getElementById('Start');
+const Info = document.getElementById('Info');
+const Last = document.getElementById('Last');
+let textCount = 0;
 
-let bars = document.querySelector("#bars")
-let sidebar = document.querySelector(".sidebar")
+chattext.addEventListener('click', function (event){
+     if(textCount == 0 & window.getComputedStyle(Who).display === 'none'){
+          Who.style.display = 'block';
+          Start.style.display = 'none';
+          textCount++;
+     }
+     else if(textCount == 1 & window.getComputedStyle(Info).display === 'none'){
+          Info.style.display = 'block';
+          Who.style.display = 'none';
+          textCount++;
+     }
+     else if(textCount == 2 & window.getComputedStyle(Last).display === 'none'){
+          Last.style.display = 'block';
+          Info.style.display = 'none';
+          textCount++;
+     }
+});
 
 
-bars.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+    
