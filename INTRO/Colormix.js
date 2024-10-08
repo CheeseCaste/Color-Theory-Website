a157802e8c0188d38f23415e77e4515dfcdf857a
CheeseCaste <elexiscastaneda@gmail.com>
2024-10-08 21:42:57 +0800

    Nice

--- INTRO/Colormix.js
+++ INTRO/Colormix.js
@@ -5,7 +5,7 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById('progress').style.width = scrolled + "%";
+  document.getElementById('myprogress').style.width = scrolled + "%";
 }
 
 //Color Mixing//
